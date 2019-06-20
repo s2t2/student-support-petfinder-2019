4555c0e26ec6db5bc42f7142613cfcac63d1d382
Mike Rossetti <s2t2@users.noreply.github.com>
2019-06-20 00:32:21 -0400

    Parse the response

--- roll_my_own.py
+++ roll_my_own.py
@@ -1,4 +1,5 @@
-from pprint import pprint
+#from pprint import pprint
+import json
 import os
 from dotenv import load_dotenv
 import requests
@@ -13,6 +14,9 @@ request_url = "https://api.petfinder.com/v2/animals"
 headers = {"Authorization": f"Bearer {TOKEN}"}
 
 response = requests.get(request_url, headers=headers)
-print(type(response))
+print(type(response)) #>
 print(response.status_code)
-pprint(response.text)
+#pprint(response.text)
+
+parsed_response = json.loads(response.text)
+print(parsed_response.keys())
